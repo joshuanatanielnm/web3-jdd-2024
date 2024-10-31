96472108f1d4e11b9ced22b206f81613fe28b7c4
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-11-01 01:32:34 +0700

    feat: create wagmi config

---
+++ src/constants/wagmiConfig.ts
@@ -0,0 +1,13 @@
+import { http, createConfig, createStorage, cookieStorage } from "wagmi";
+import { sepolia } from "wagmi/chains";
+
+export const config = createConfig({
+  chains: [sepolia],
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  transports: {
+    [sepolia.id]: http(),
+  },
+});
